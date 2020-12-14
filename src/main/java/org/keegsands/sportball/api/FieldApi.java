5b335ad51b1ec445cc29f29e9031831793aa7540
Keegan Sands <keegsands@gmail.com>
2020-12-13 21:29:24 -0500

    Filling out api
    Added new game, team and season api
    Added game times to the field api

--- src/main/java/org/keegsands/sportball/api/FieldApi.java
+++ src/main/java/org/keegsands/sportball/api/FieldApi.java
@@ -3,7 +3,9 @@ package org.keegsands.sportball.api;
 import java.util.List;
 
 import org.keegsands.sportball.model.Field;
+import org.keegsands.sportball.model.GameTime;
 import org.keegsands.sportball.service.FieldService;
+import org.keegsands.sportball.service.GameTimeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +20,14 @@ public class FieldApi {
 	private static final String GET_FIELDS = "/api/fields";
 	private static final String GET_FIELD_BY_ID = "/api/fields/{fieldId}";
 	
+	private static final String GET_GAME_TIMES = "/api/gameTimes";
+	private static final String GET_GAME_TIME_BY_ID = "/api/gameTimes/{gameTimeId}";
+	
 	@Autowired
 	private FieldService fieldService;
+	
+	@Autowired
+	private GameTimeService gameTimeService;
 	
 	public FieldApi() {
 		System.out.println("Starting the field API");
@@ -34,5 +42,16 @@ public class FieldApi {
 	public ResponseEntity<Field> getFieldById(@PathVariable int fieldId) {
 		final Field field = fieldService.getById(fieldId);
 		return new ResponseEntity<>(field, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value=GET_GAME_TIMES, method = RequestMethod.GET)
+	public ResponseEntity<List<GameTime>> getGameTimes() {
+		return new ResponseEntity<>(gameTimeService.list(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value=GET_GAME_TIME_BY_ID, method = RequestMethod.GET)
+	public ResponseEntity<GameTime> getGameTimeById(@PathVariable int gameTimeId) {
+		final GameTime gameTime = gameTimeService.getById(gameTimeId);
+		return new ResponseEntity<>(gameTime, HttpStatus.OK);
 	}
 }
