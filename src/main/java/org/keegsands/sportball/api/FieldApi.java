41b41f726c6abd06b263d9cfa7597dee40e18c20
Keegan Sands <keegsands@gmail.com>
2020-12-13 20:37:57 -0500

    New version
    Switch to 1.1.0
    Update DAO to get instead of load
    Added new get by id method to Field API

--- src/main/java/org/keegsands/sportball/api/FieldApi.java
+++ src/main/java/org/keegsands/sportball/api/FieldApi.java
@@ -7,13 +7,16 @@ import org.keegsands.sportball.service.FieldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/fields")
 public class FieldApi {
+	
+	private static final String GET_FIELDS = "/api/fields";
+	private static final String GET_FIELD_BY_ID = "/api/fields/{fieldId}";
 	
 	@Autowired
 	private FieldService fieldService;
@@ -22,8 +25,14 @@ public class FieldApi {
 		System.out.println("Starting the field API");
 	}
 
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value=GET_FIELDS, method = RequestMethod.GET)
 	public ResponseEntity<List<Field>> getAllFields() {
 		return new ResponseEntity<>(fieldService.list(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value=GET_FIELD_BY_ID, method = RequestMethod.GET)
+	public ResponseEntity<Field> getFieldById(@PathVariable int fieldId) {
+		final Field field = fieldService.getById(fieldId);
+		return new ResponseEntity<>(field, HttpStatus.OK);
 	}
 }
